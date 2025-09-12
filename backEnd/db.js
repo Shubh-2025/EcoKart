816b5d6e869f0511f0e4d387c3e9493be47cf66c
Shubhranil Karmakar <samratkarmakarnaihati@gmail.com>
2025-09-13 00:34:39 +0530

    bug fix

--- backEnd/db.js
+++ backEnd/db.js
@@ -1,7 +1,6 @@
 import pkg from "pg";
 // dotenv import
 // dotenv config
-
 const { Pool } = pkg;
 const pool = new Pool ({
     connectionString :"postgresql://postgres.jhwyjifvojriwvdoyinp:ShubhranilSupabase@aws-1-ap-south-1.pooler.supabase.com:6543/postgres", //supabase key
