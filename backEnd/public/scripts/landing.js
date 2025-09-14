3ac66f17ef580e22d5eaeee0d09d9fc3908b0075
Shubh-2025 <samratkarmakarnaihati@gmail.com>
2025-09-14 14:33:08 +0530

    cart functionality  added

--- backEnd/public/scripts/landing.js
+++ backEnd/public/scripts/landing.js
@@ -7,21 +7,26 @@ async function getItems() {
       container.innerHTML = "<h1> No Items Found...</h1>";
     } else {
       data = await response.json();
-      // console.log(data);
       let itemarray = data.message;
-      console.log(itemarray);
+      // console.log(itemarray);
       for (let i of itemarray) {
         let div1 = document.createElement("div");
+        div1.id = i.id;
+        div1.addEventListener("click", (e) => {
+          localStorage.setItem("id", JSON.parse(e.currentTarget.id));
+          // this way the element which has the eventlistner will be the target..not the children
+          window.location.href = "/EcoKart/product";
+        });
         div1.className =
-          "bg-white rounded-lg shadow-xl overflow-hidden flex flex-col items-center h-70 sm:h-100 md:h-100 cursor-pointer";
+          "bg-white rounded-lg shadow-xl overflow-hidden flex flex-col items-center h-80 sm:h-100 md:h-100 cursor-pointer";
         let img = document.createElement("img");
         img.src = i.imageurl;
         img.alt = "item image";
-        img.className = "w-full h-70 object-cover mb-4";
+        img.className = "w-full h-70 object-cover mb-2";
         let h4 = document.createElement("h4");
         h4.textContent = i.name;
         h4.className =
-          "text-md sm:text-lg md:text-lg font-semibold text-green-700 mb-2";
+          "text-md sm:text-lg md:text-lg font-semibold text-green-700 m-2";
         let h3 = document.createElement("h3");
         h3.className = "text-lg font-bold text-green-500 mb-2";
         let sp1 = document.createElement("span");
@@ -30,13 +35,13 @@ async function getItems() {
         sp2.textContent = i.price; //price
         h3.appendChild(sp1);
         h3.appendChild(sp2);
-        let p = document.createElement("p");
-        p.className = "text-green-800 mb-4 text-center";
-        p.textContent = i.description;
+        // let p = document.createElement("p");
+        // p.className = "text-green-800 mb-4 text-center";
+        // p.textContent = i.description;
         div1.appendChild(img);
         div1.appendChild(h4);
         div1.appendChild(h3);
-        div1.appendChild(p);
+        // div1.appendChild(p);
 
         // for each loop the items are appended here
         container.appendChild(div1);
